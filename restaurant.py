fff33e2bf63b63a3d5c62e62ebba23b980e148f9
2101330hayasi <113326147+2101330hayasi@users.noreply.github.com>
2022-12-07 13:33:34 +0900

    restaurant.pyを編集
    APIのリクエスト送信部分を書いた

--- restaurant.py
+++ restaurant.py
@@ -2,7 +2,22 @@
 import recipe
 
 # レストランのデータを取ってくる処理
-def get_restaurant(){
-
+import requests
+import json
+def get_restaurant(key,keyword):
     
-}
+ 
+ 
+    #urlの作成
+    urlbase = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=sample&large_area=Z011' #ランキングAPIのベースとなるURL
+ 
+    parameters = {
+                'key' : key,  #APIを利用するために割り当てられたキーを設定します。
+                'keyword' : keyword, #店名かな、店名、住所、駅名、お店ジャンルキャッチ、キャッチのフリーワード検索(部分一致)が可能です.
+                # 'range' : range, #ある地点からの範囲内のお店の検索を行う場合の範囲を5段階で指定できます。
+                # 'lat' : lat, #ある地点からの範囲内のお店の検索を行う場合の緯度です。
+                # 'lng' : lng, #ある地点からの範囲内のお店の検索を行う場合の経度です。
+
+
+    }
+ 

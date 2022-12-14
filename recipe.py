61e7fe33cecf863642fa4d883867fb1c41302cfb
aso2101340 <abcc2101340@gmail.com>
2022-12-14 11:57:38 +0900

    Update recipe.py
    一旦消した

--- recipe.py
+++ recipe.py
@@ -1,50 +1,10 @@
-
-#def get_large_category():
-L
-#def get_middle_category():
-
-#def get_recipe():
-
 import requests
 import json
-import time
+import env
 import pandas as pd
-from pprint import pprint
+from req import req
 
-# 1. 楽天レシピのレシピカテゴリ一覧を取得する
+res = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=')
 
-res = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=xxxxx')
+key = env.get_hp_key()
 
-json_data = json.loads(res.text)
-
-parent_dict = {} # mediumカテゴリの親カテゴリの辞書
-
-df = pd.DataFrame(columns=['category1','category2','category3','categoryId','categoryName'])
-
-for category in json_data['result']['large']:
-    df = df.append({'category1':category['categoryId'],'category2':"",'category3':"",'categoryId':category['categoryId'],'categoryName':category['categoryName']}, ignore_index=True)
-
-for category in json_data['result']['medium']:
-    df = df.append({'category1':category['parentCategoryId'],'category2':category['categoryId'],'category3':"",'categoryId':str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, ignore_index=True)
-    parent_dict[str(category['categoryId'])] = category['parentCategoryId']
-
-for category in json_data['result']['small']:
-    df = df.append({'category1':parent_dict[category['parentCategoryId']],'category2':category['parentCategoryId'],'category3':category['categoryId'],'categoryId':parent_dict[category['parentCategoryId']]+"-"+str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, ignore_index=True)
-
-# 2. キーワードからカテゴリを抽出する
-df_keyword = df.query('categoryName.str.contains("魚")', engine='python')
-
-# 3. 人気レシピを取得する
-df_recipe = pd.DataFrame(columns=['recipeId', 'recipeTitle', 'foodImageUrl', 'recipeMaterial', 'recipeCost', 'recipeIndication', 'categoryId', 'categoryName'])
-
-for index, row in df_keyword.iterrows():
-    time.sleep(3) # 連続でアクセスすると先方のサーバに負荷がかかるので少し待つのがマナー
-
-    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=xxxxx&categoryId='+row['categoryId']
-    res = requests.get(url)
-
-    json_data = json.loads(res.text)
-    recipes = json_data['result']
-
-    for recipe in recipes:
-        df_recipe = df_recipe.append({'recipeId':recipe['recipeId'],'recipeTitle':recipe['recipeTitle'],'foodImageUrl':recipe['foodImageUrl'],'recipeMaterial':recipe['recipeMaterial'],'recipeCost':recipe['recipeCost'],'recipeIndication':recipe['recipeIndication'],'categoryId':row['categoryId'],'categoryName':row['categoryName']}, ignore_index=True)

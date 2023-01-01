b88c96dc9b8d1b74663f6d8f14d5b6b2e2dbdc71
aso2101340 <abcc2101340@gmail.com>
2023-01-01 22:42:27 +0900

    Update recipe.py
    一覧全部でるようになりました

--- recipe.py
+++ recipe.py
@@ -1,19 +1,22 @@
 import requests
 import json
 import env
+import time
 import pandas as pd
 from req import req
 
+res = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=')
 key = env.get_rak_key()
 
-# def get_l_cat():
-#     url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key + '&categoryType=large'
-#     return req(url) 
+def get_l_cat():
+    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key + '&categoryType=large'
+    return req(url) 
 
 
-# res = get_l_cat()
-# for i in range(len(res['result']['large'])):
-#     print(res['result']['large'][i]['categoryId'],res['result']['large'][i]['categoryName'])
+res = get_l_cat()
+for i in range(len(res['result']['large'])):
+    time.sleep(0.05)
+    print(res['result']['large'][i]['categoryId'],res['result']['large'][i]['categoryName'])
 
 def get_m_cat():
     url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key + '&categoryType=medium'
@@ -22,15 +25,16 @@ def get_m_cat():
 res = get_m_cat()
 #print(res['result']['medium'][0])
 for i in range(len(res['result']['medium'])):
+    time.sleep(0.05)
     print(res['result']['medium'][i]['categoryId'],res['result']['medium'][i]['categoryName'])
 
-# def get_s_cat():
-#     url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key + '&categoryType=small'
-#     return req(url)
+def get_s_cat():
+    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key + '&categoryType=small'
+    return req(url)
 
-# res = get_s_cat()
-# print(res['result']['small'][0])
-# for i in range(len(res['result']['small'])):
-#     print(res['result']['small'][i]['categoryId'],res['result']['small'][i]['categoryName'])
+res = get_s_cat()
+print(res['result']['small'][0])
+for i in range(len(res['result']['small'])):
+    time.sleep(0.05)
+    print(res['result']['small'][i]['categoryId'],res['result']['small'][i]['categoryName'])
 
-#m_res = req()

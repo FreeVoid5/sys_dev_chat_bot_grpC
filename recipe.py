6ed22efd20411ab5e04f1f2b0d2154d17727de69
aso2101340 <abcc2101340@gmail.com>
2023-01-03 19:10:12 +0900

    Update recipe.py
    キーワード入れたら出るようになりました

--- recipe.py
+++ recipe.py
@@ -5,36 +5,44 @@ import time
 import pandas as pd
 from req import req
 
-res = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=')
 key = env.get_rak_key()
 
 def get_l_cat():
-    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key + '&categoryType=large'
+    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key
     return req(url) 
 
 res = get_l_cat()
-for i in range(len(res['result']['large'])):
-    print(res['result']['large'][i]['categoryId'],res['result']['large'][i]['categoryName'])
+# mediumカテゴリの親カテゴリの辞書
+parent_dict = {}
 
-time.sleep(1)
+df = pd.DataFrame(columns=['category1','category2','category3','categoryId','categoryName'])
 
-def get_m_cat():
-    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key + '&categoryType=medium'
-    return req(url)
+# 大カテゴリ
+for category in res['result']['large']:
+    df = df.append({'category1':category['categoryId'],'category2':"",'category3':"",'categoryId':category['categoryId'],'categoryName':category['categoryName']}, ignore_index=True)
 
-res = get_m_cat()
-#print(res['result']['medium'][0])
-for i in range(len(res['result']['medium'])):
-    print(res['result']['medium'][i]['categoryId'],res['result']['medium'][i]['categoryName'])
+# 中カテゴリ
+for category in res['result']['medium']:
+    df = df.append({'category1':category['parentCategoryId'],'category2':category['categoryId'],'category3':"",'categoryId':str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, ignore_index=True)
+    parent_dict[str(category['categoryId'])] = category['parentCategoryId']
 
-time.sleep(1)
+# 小カテゴリ
+for category in res['result']['small']:
+    df = df.append({'category1':parent_dict[category['parentCategoryId']],'category2':category['parentCategoryId'],'category3':category['categoryId'],'categoryId':parent_dict[category['parentCategoryId']]+"-"+str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, ignore_index=True)
 
-def get_s_cat():
-    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=' + key + '&categoryType=small'
-    return req(url)
+df_keyword = df.query('categoryName.str.contains("麻婆豆腐")', engine='python')
 
-res = get_s_cat()
-print(res['result']['small'][0])
-for i in range(len(res['result']['small'])):
-    print(res['result']['small'][i]['categoryId'],res['result']['small'][i]['categoryName'])
+df_recipe = pd.DataFrame(columns=['recipeId', 'recipeTitle', 'foodImageUrl', 'recipeMaterial', 'recipeCost', 'recipeIndication', 'categoryId', 'categoryName'])
 
+for index, row in df_keyword.iterrows():
+    time.sleep(1) # 連続でアクセスすると先方のサーバに負荷がかかるので少し待つのがマナー
+
+    url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=' + key + '&categoryId='+row['categoryId']
+    res = requests.get(url)
+
+    json_data = json.loads(res.text)
+    recipes = json_data['result']
+
+    for recipe in recipes:
+        df_recipe = df_recipe.append({'recipeId':recipe['recipeId'],'recipeTitle':recipe['recipeTitle'],'foodImageUrl':recipe['foodImageUrl'],'recipeMaterial':recipe['recipeMaterial'],'recipeCost':recipe['recipeCost'],'recipeIndication':recipe['recipeIndication'],'categoryId':row['categoryId'],'categoryName':row['categoryName']}, ignore_index=True)
+        print(df_recipe)
